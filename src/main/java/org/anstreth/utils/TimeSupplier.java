0cb81b43b172961026def0d723b4b7fd6c713ce1
Roman Golyshev <anstreth@gmail.com>
2017-09-28 02:40:22 +0300

    #16-timezone-issue: add timezone bot property

--- src/main/java/org/anstreth/utils/TimeSupplier.java
+++ src/main/java/org/anstreth/utils/TimeSupplier.java
@@ -1,13 +1,22 @@
 package org.anstreth.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 import java.util.TimeZone;
 
 @Component
+@PropertySource("classpath:bot.properties")
 public class TimeSupplier {
-    private final TimeZone TARGET_TIMEZONE = TimeZone.getTimeZone("Russia/Moscow");
+    private final TimeZone TARGET_TIMEZONE;
+
+    @Autowired
+    TimeSupplier(@Value("${bot.target-timezone}") String timezoneName) {
+        TARGET_TIMEZONE = TimeZone.getTimeZone(timezoneName);
+    }
 
     public Calendar now() {
         return Calendar.getInstance(TARGET_TIMEZONE);
